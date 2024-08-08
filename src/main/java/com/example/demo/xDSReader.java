0f91de18f5967946551052894fba52e757aa4f2d
donghoon_han <donghoon_han@tmaxsoft.co.kr>
2024-08-08 10:07:47 +0900

    start

--- src/main/java/com/example/demo/xDSReader.java
+++ src/main/java/com/example/demo/xDSReader.java
@@ -1,4 +1,10 @@
 package com.example.demo;
 
 public class xDSReader {
+    // type URL
+    // -> while, loop out : connection fail
+    // onNext?
+    // can i know there is any sign when it is changed?
+    //
+
 }
